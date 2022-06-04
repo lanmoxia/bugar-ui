afc702e81760eda9d2fb83061bb7c3ca18d17f36
lifeng <506076395@qq.com>
2022-06-04 17:35:58 +0800

    Home 和 Doc 组件 HTML 完成

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,4 @@
 <template>
-<!--  vue3 的 template 支持多个标签-->
-  <div>
-    导航栏 |
-    <router-link to="/">Test</router-link>
-    |
-    <router-link to="/test2">Test2</router-link>
-  </div>
-  <hr/>
   <router-view/>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import {createWebHashHistory, createRouter} from 'vue-router'
-import Test from './components/Test.vue'
-import Test2 from './components/Test2.vue'
-// 入口文件
+import Home from './views/Home.vue'
+import Doc from './views/Doc.vue'
+
 const history = createWebHashHistory()
-// main.js 改为 main.ts 文件 提示应该有一个参数
-// 通过 ctrl 点击 createRouter 发现类型是 RouterOptions
-// 再点击往下看到 history routes 这些属性 应该就是我们定义的 history 和 router
-// 参数应该是一个对象 {history 对应我们选的 hash 模式 routes 应该是一个数组(不同的路由)}
 const router = createRouter({
   history: history,
   routes: [
-    {path: '/', component: Test},
-    {path: '/test2', component: Test2}
+    {path: '/', component: Home},
+    {path: '/doc', component: Doc}
   ]
 })
 const app = createApp(App)
