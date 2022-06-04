51303a9ff3081fa69f97c77c3dec03364ddb498f
lifeng <506076395@qq.com>
2022-06-04 22:11:09 +0800

    路由切换之后关闭 aside

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,20 @@
 <script lang="ts">
 
 import {provide, ref} from 'vue';
+import {router} from './router';
 export default {
   name: 'App',
   setup(){
     // 获取当前页面宽度
     const width = document.documentElement.clientWidth
-    const menuVisible = ref(width <= 500 ? false : true)
+    const menuVisible = ref(width > 500)
     provide('menuVisible', menuVisible) // 相当于是 set
+    // 移动端路由变化了就把 menuVisible 的值改为 false
+    router.afterEach(() => {
+      if(width <= 500){
+        menuVisible.value = false
+      }
+    })
   }
 }
 </script>
