4ffb707bd531930b8bf1ac7af99326763ef20955
lifeng <506076395@qq.com>
2022-06-04 16:21:12 +0800

    安装 vue-router

--- src/main.js
+++ src/main.js
@@ -2,7 +2,4 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 
-// vue3 是 createApp(App)  vue2 是 new Vue()
-// vue3 的 createApp(App) 接受的是组件
-// vue2 的 new vue({template,render}) 接收的是对象
 createApp(App).mount('#app')
