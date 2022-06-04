7c06c7ab7020e493e264a5e048c2b95e30f30794
lifeng <506076395@qq.com>
2022-06-04 16:44:13 +0800

    初始化 router：2.新建 router 对象 引入 ts

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.css'
-import {createWebHashHistory} from 'vue-router'
-const history = createWebHashHistory()
-
-createApp(App).mount('#app')
---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './index.css'
+import {createWebHashHistory, createRouter} from 'vue-router'
+import Test from './components/Test.vue'
+
+// 入口文件
+const history = createWebHashHistory()
+// main.js 改为 main.ts 文件 提示应该有一个参数
+// 通过 ctrl 点击 createRouter 发现类型是 RouterOptions
+// 再点击往下看到 history routes 这些属性 应该就是我们定义的 history 和 router
+// 参数应该是一个对象 {history 对应我们选的 hash 模式 routes 应该是一个数组(不同的路由)}
+const router = createRouter({
+  history: history,
+  routes: [
+    {path: '/', component: Test}
+  ]
+})
+createApp(App).mount('#app')
