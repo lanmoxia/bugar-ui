119bdb04d655177f665e923716d28a044852940b
lifeng <506076395@qq.com>
2022-06-04 20:08:32 +0800

    通过页面宽度判断 menuVisible 的默认值

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,9 @@ import {provide, ref} from 'vue';
 export default {
   name: 'App',
   setup(){
-    const menuVisible = ref(false)
+    // 获取当前页面宽度
+    const width = document.documentElement.clientWidth
+    const menuVisible = ref(width <= 500 ? false : true)
     provide('menuVisible', menuVisible) // 相当于是 set
   }
 }
