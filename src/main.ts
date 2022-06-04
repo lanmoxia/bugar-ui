39e1084c714b0e9ac1855fea669e194fdf580f45
lifeng <506076395@qq.com>
2022-06-04 20:34:08 +0800

    点击切换子路由,隐藏 aside 受阻

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,14 @@ const router = createRouter({
       ]}
   ]
 })
+// 通过 router.afterEach 能知道路由有没有切换
+// 这样切换了可以改变 menuVisible 的值
+// 因为 menuVisible 变量在 App 中 无法访问到
+// 把 router.afterEach 放到 App 中 这样就能访问到 menuVisible
+// 但是 App 访问不到 router
+router.afterEach(() => {
+  console.log('路由切换了');
+})
 const app = createApp(App)
 app.use(router) // 使用 app.use
 app.mount('#app')
