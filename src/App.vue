519b2b949d8ffc16e197c90508fc604d423ce829
lifeng <506076395@qq.com>
2022-06-04 13:12:14 +0800

    初始化(vue2和vue3的不同)

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+<!--  vue3 的 template 支持多个标签-->
+  <div>hi</div>
+  <Test/>
+</template>
+
+<script>
+import Test from './components/Test.vue'
+export default {
+  name: 'App',
+  components: {
+    Test
+  }
+}
+</script>
---
+++ src/components/Test.vue
@@ -0,0 +1,5 @@
+  <template>
+    <div>
+      这是我第一个组件
+    </div>
+  </template>
