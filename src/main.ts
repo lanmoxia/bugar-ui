237644b3170b366cf494d53f92ff9789f4762127
lifeng <506076395@qq.com>
2022-06-04 22:02:22 +0800

    把 router 封装成一个单独的文件

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.scss'
-import {createWebHashHistory, createRouter} from 'vue-router'
-import Home from './views/Home.vue'
-import Doc from './views/Doc.vue'
-import SwitchDemo from './components/SwitchDemo.vue'
-const history = createWebHashHistory()
-const router = createRouter({
-  history: history,
-  routes: [
-    {path: '/', component: Home},
-    {path: '/doc', component: Doc, children:[
-        {path: 'switch', component: SwitchDemo}
-      ]}
-  ]
-})
-// 通过 router.afterEach 能知道路由有没有切换
-// 这样切换了可以改变 menuVisible 的值
-// 因为 menuVisible 变量在 App 中 无法访问到
-// 把 router.afterEach 放到 App 中 这样就能访问到 menuVisible
-// 但是 App 访问不到 router
-router.afterEach(() => {
-  console.log('路由切换了');
-})
+import {router} from './router'
+
 const app = createApp(App)
 app.use(router) // 使用 app.use
 app.mount('#app')
---
+++ src/router.ts
@@ -0,0 +1,24 @@
+import Home from './views/Home.vue'
+import Doc from './views/Doc.vue'
+import SwitchDemo from './components/SwitchDemo.vue'
+import {createRouter, createWebHashHistory} from 'vue-router';
+
+const history = createWebHashHistory()
+export const router = createRouter({
+  history: history,
+  routes: [
+    {path: '/', component: Home},
+    {path: '/doc', component: Doc, children:[
+        {path: 'switch', component: SwitchDemo}
+      ]}
+  ]
+})
+// 通过 router.afterEach 能知道路由有没有切换
+// 这样切换了可以改变 menuVisible 的值
+// 因为 menuVisible 变量在 App 中 无法访问到
+// 把 router.afterEach 放到 App 中 这样就能访问到 menuVisible
+// 但是 App 访问不到 router
+// 所以新建文件把 router 的相关放到这里
+router.afterEach(() => {
+  console.log('路由切换了');
+})
