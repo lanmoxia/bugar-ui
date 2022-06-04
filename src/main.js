27383832229fe6ccdc477aab5d85ed414f99b17e
lifeng <506076395@qq.com>
2022-06-04 16:23:08 +0800

    初始化 router：1.新建 history 对象使用 hashHistory 模式

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import {createWebHashHistory} from 'vue-router'
+const history = createWebHashHistory()
 
 createApp(App).mount('#app')
