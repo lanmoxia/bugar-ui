81ef12f0706ef951be22f928d6b583d5fee5e7c2
lifeng <506076395@qq.com>
2022-06-04 20:00:19 +0800

    aside 按钮完成显示隐藏切换

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="topNav">
-    <div class="logo" @click="toggleMenu">LOGO</div>
+    <div class="logo">LOGO</div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
     </ul>
-    <span class="toggleAside"></span>
+    <span class="toggleAside" @click="toggleMenu"></span>
   </div>
 </template>
 
@@ -44,18 +44,20 @@ import {inject, Ref} from 'vue';
     }
   }
   >.toggleAside{
-    display: inline-block;
     width: 24px;
     height: 24px;
     background: red;
+    border-radius: 50%;
     position: absolute;
     left: 16px;
     top: 50%;
     transform: translateY(-50%);
+    display: none;
   }
   @media (max-width:500px){
     >.menu{display: none;}
     >.logo{margin: 0 auto;}
+    >.toggleAside{display: inline-block;}
   }
 }
 </style>
