8ea47fb99a039f996aa823e7ae182288b2d044ff
lifeng <506076395@qq.com>
2022-06-04 20:17:58 +0800

    嵌套路由点击进入子路由

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,15 @@ import './index.scss'
 import {createWebHashHistory, createRouter} from 'vue-router'
 import Home from './views/Home.vue'
 import Doc from './views/Doc.vue'
-
+import SwitchDemo from './components/SwitchDemo.vue'
 const history = createWebHashHistory()
 const router = createRouter({
   history: history,
   routes: [
     {path: '/', component: Home},
-    {path: '/doc', component: Doc}
+    {path: '/doc', component: Doc, children:[
+        {path: 'switch', component: SwitchDemo}
+      ]}
   ]
 })
 const app = createApp(App)
