8bc3c1e015e4885a7a4deddb3518c265db0b5afa
lifeng <506076395@qq.com>
2022-06-04 22:57:38 +0800

    添加 Doc 默认页面 官网基本完成

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,6 @@
 import Home from './views/Home.vue'
 import Doc from './views/Doc.vue'
+import DocDemo from './components/DocDemo.vue'
 import SwitchDemo from './components/SwitchDemo.vue'
 import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
@@ -12,6 +13,7 @@ export const router = createRouter({
   routes: [
     {path: '/', component: Home},
     {path: '/doc', component: Doc, children:[
+        {path: '', component: DocDemo},
         {path: 'switch', component: SwitchDemo},
         {path: 'button', component: ButtonDemo},
         {path: 'dialog', component: DialogDemo},
