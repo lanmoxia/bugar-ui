ea41f75820b4a09203b665fcdc1d772595a46953
lifeng <506076395@qq.com>
2022-06-04 20:03:59 +0800

    移动端点击显示aside切换到PC端依然显示

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -39,10 +39,7 @@ export default {
   aside {
     background: lightblue;
     width: 150px;
-    position: fixed;
-    top: 0;
-    left: 0;
-    padding: 70px 16px 16px;
+    padding: 16px;
     > h2 {
       margin-bottom: 4px;
     }
@@ -50,6 +47,12 @@ export default {
       > li {
         padding: 4px 0;
       }
+    }
+    @media (max-width: 500px) {
+      position: fixed;
+      top: 0;
+      left: 0;
+      padding-top: 70px;
     }
   }
 </style>
