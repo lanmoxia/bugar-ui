a05a162e1f79059c05988eb624e8bd38ba9c5790
lifeng <506076395@qq.com>
2022-06-05 12:28:01 +0800

    Switch 小件布局样式

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
-    Switch 示例文档
+    <Switch />
   </div>
 </template>
+
+<script lang="ts">
+import Switch from '../lib/Switch.vue';
+
+export default {
+  components: {Switch}
+};
+</script>
