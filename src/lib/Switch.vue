fbf0f00a873e1e3f38c4f207bf8add9228810d3a
lifeng <506076395@qq.com>
2022-06-05 12:44:14 +0800

    Switch 小件 点击可以切换

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,6 +1,21 @@
 <template>
-  <button><span></span></button>
+  <button @click="toggle" :class="{checked: checked}">
+    <span></span>
+  </button>
 </template>
+
+<script lang="ts">
+import {ref} from 'vue';
+export default {
+  setup(){
+    const checked = ref(false)
+    const toggle = () => {
+      checked.value = !checked.value
+    }
+    return{checked:checked, toggle: toggle}
+  }
+}
+</script>
 
 <style lang="scss" scoped>
   $h: 22px;
@@ -9,7 +24,7 @@
     height: $h;
     width: $h*2;
     border: none;
-    background: blue;
+    background: gray;
     border-radius: $h/2;
     position: relative;
   }
@@ -22,7 +37,10 @@
     background: white;
     border-radius: $h2 / 2;
   }
-  button:hover > span {
+  button.checked {
+    background: blue;
+  }
+  button.checked > span {
     left: calc(100% - #{$h2} - 2px);
   }
 </style>
