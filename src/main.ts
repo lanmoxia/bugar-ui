9483941b190e9ae472668c21838ae92e74eb45e2
lifeng <506076395@qq.com>
2022-06-06 21:11:23 +0800

    作为 UI 库：CSS 最小影响原则

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
+// 必须放到前边
+import './lib/gugu.scss'
+import './index.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
-import './index.scss'
 import {router} from './router'
 
 const app = createApp(App)
