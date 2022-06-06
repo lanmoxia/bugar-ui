2d28bc166a0ce19c5602cc7e12f2daf36c9bd2cc
lifeng <506076395@qq.com>
2022-06-06 13:14:13 +0800

    V3属性绑定1

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -3,13 +3,21 @@
     <div>Button 示例</div>
     <h1>示例1</h1>
     <div>
-      <Button>你好</Button>
+      <Button @click="xxx"
+        @focus="xxx" @mouseover="xxx"
+      >你好</Button>
     </div>
   </div>
 </template>
 <script lang="ts">
 import Button from '../lib/Button.vue'
 export default {
-  components: {Button}
+  components: {Button},
+  setup(){
+    const xxx = () => {
+      console.log('hi');
+    }
+    return {xxx}
+  }
 }
 </script>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,4 +1,5 @@
 <template>
+  <!--vue 会把外部的事件传到这里的根元素实现事件绑定-->
   <button>
     <slot/>
   </button>
