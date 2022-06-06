5fd3013ebfce931a8fcde37523181ef28e0f2326
lifeng <506076395@qq.com>
2022-06-07 00:14:54 +0800

    新建 Dialog 组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
-    Dialog 示例文档
-  </div>
+  <div>Dialog 示例</div>
+  <h1>示例1</h1>
+    <Dialog></Dialog>
 </template>
+<script>
+import Dialog from '../lib/Dialog.vue'
+export default {
+  components: {Dialog}
+}
+</script>
