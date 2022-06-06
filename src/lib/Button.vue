b67b9c97fec2d193daf5be98fe3ccfe7f60a4156
lifeng <506076395@qq.com>
2022-06-06 21:51:57 +0800

    Button 支持 level 属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -17,14 +17,19 @@ export default {
     size: {
       type: String,
       default: 'normal'
+    },
+    level: {
+      type: String,
+      default: 'normal'
     }
   },
   setup(props){
-    const {theme, size} = props
+    const {theme, size, level} = props
     const classes = computed(() => {
       return {
         [`gugu-theme-${theme}`]: theme,
-        [`gugu-size-${size}`]: size
+        [`gugu-size-${size}`]: size,
+        [`gugu-level-${level}`]: level
       }
     })
     return {classes}
@@ -38,6 +43,7 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
 .gugu-button {
   box-sizing: border-box;
   height: $h;
@@ -52,6 +58,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
   //&+& 是 gugu-button+gugu-button 的意思
   & + & {
     margin-left: 8px;
@@ -92,6 +99,53 @@ $radius: 4px;
     font-size: 12px;
     height: 20px;
     padding: 0 4px;
+  }
+  &.gugu-theme-button {
+    &.gugu-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+    &.gugu-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+  &.gugu-theme-link {
+    &.gugu-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.gugu-theme-text {
+    &.gugu-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.gugu-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
   }
 }
 </style>
