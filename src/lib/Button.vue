9f587d54c6a390056eb6a449a09095ef304f2560
lifeng <506076395@qq.com>
2022-06-06 21:14:24 +0800

    Button 支持 theme 属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -52,5 +52,21 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
+  &.gugu-theme-link{
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+    &:hover,&:focus{
+      color: lighten($blue, 10%);
+    }
+  }
+  &.gugu-theme-text{
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover,&:focus{
+      background: darken(white, 5%);;
+    }
+  }
 }
 </style>
