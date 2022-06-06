eb8321351bc97f536c405f991567ccb5b7cd3e98
lifeng <506076395@qq.com>
2022-06-06 19:35:57 +0800

    第二种解决方式：没有传类名就显示了

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-    <!--共有类名和动态类名-->
-    <button class="gugu-button" :class="`theme-${theme}`">
+    <!--第二种解决办法使用 :class="{[`theme-${theme}`]: theme}"-->
+    <!--如果 ${theme} 不存在就不用显示了-->
+    <button class="gugu-button"
+            :class="{[`theme-${theme}`]: theme}">
       <slot/>
     </button>
   </div>
@@ -12,8 +14,7 @@ export default {
   // 这样设置默认值 可以解决第一个 button 是 theme-undefined 的问题
   props: {
     theme: {
-      type: String,
-      default: 'button'
+      type: String
     }
   }
 }
