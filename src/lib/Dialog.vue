8de9bcf8ca2a25b93e4e38c39616d91a68d27ba5
lifeng <506076395@qq.com>
2022-06-07 15:07:46 +0800

    Teleport标签：传送门，可以让子元素摆脱父元素传送到其他元素下，脱离当前层叠环境。

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,21 +1,23 @@
 <template>
   <template v-if="visible">
-    <div class="gugu-dialog-overlay" @click="onClickOverlay"></div>
-    <div class="gugu-dialog-wrapper">
-      <div class="gugu-dialog">
-        <header>
-          <slot name="title"/>
-          <span @click="close" class="gugu-dialog-close"></span>
-        </header>
-        <main>
-          <slot name="content"/>
-        </main>
-        <footer>
-          <Button level="main" @click="ok">OK</Button>
-          <Button @click="cancel">Cancel</Button>
-        </footer>
+    <Teleport to="body">
+      <div class="gugu-dialog-overlay" @click="onClickOverlay"></div>
+      <div class="gugu-dialog-wrapper">
+        <div class="gugu-dialog">
+          <header>
+            <slot name="title"/>
+            <span @click="close" class="gugu-dialog-close"></span>
+          </header>
+          <main>
+            <slot name="content"/>
+          </main>
+          <footer>
+            <Button level="main" @click="ok">OK</Button>
+            <Button @click="cancel">Cancel</Button>
+          </footer>
+        </div>
       </div>
-    </div>
+    </Teleport>
   </template>
 </template>
 
