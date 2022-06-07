479d5ff25530b9d8341415102ed06cd42750a127
lifeng <506076395@qq.com>
2022-06-07 23:39:01 +0800

    Tabs 样式完成

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,9 +1,11 @@
 <template>
-  <div>
-    <div>
-      <div v-for="(t, index) in titles" :key="index">{{t}}</div>
+  <div class="gugu-tabs">
+    <div class="gugu-tabs-nav">
+      <div class="gugu-tabs-nav-item" v-for="(t, index) in titles" :key="index">{{t}}</div>
     </div>
-    <component v-for=" (c, index) in defaults" :key="index" :is="c"/>
+    <div class="gugu-tabs-content div">
+      <component v-for=" (c, index) in defaults" :key="index" :is="c"/>
+    </div>
   </div>
 </template>
 
@@ -27,3 +29,30 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.gugu-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
