b13cec55c658f89a828d038c1c9d6fa6190dcab7
lifeng <506076395@qq.com>
2022-06-07 17:03:48 +0800

    动态挂载组件2：使用 vue 的 h函数 来传 visible 属性

--- src/lib/openDialog.ts
+++ src/lib/openDialog.ts
@@ -1,5 +1,5 @@
 import Dialog from './Dialog.vue'
-import {createApp} from 'vue'; //提供挂载的 API
+import {createApp, h} from 'vue'; //提供挂载的 API
 export const openDialog = (options) => {
   const {title, content} = options
   // 直接把 Dialog 放到 body 中会覆盖之前的
@@ -7,6 +7,11 @@ export const openDialog = (options) => {
   const div = document.createElement('div')
   document.body.appendChild(div)
   // 把 Dialog 挂载到 div 上
-  createApp(Dialog).mount(div)
+  createApp({
+    render(){
+      return h(Dialog, {visible: true})
+    }
+  }).mount(div)
 }
-//挂载 Dialog 由于 visible 默认是 false，这里没有传属性所以点击无效
+// 解决点击无反应：使用 vue的h函数，h函数能接受多个参数
+// 可以把 visible 属性也传过去，这样就解决了点击无反应的问题
