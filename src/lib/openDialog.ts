63c791d924c4bd7fc3d6542d789ae30f55d88f1a
lifeng <506076395@qq.com>
2022-06-07 18:09:29 +0800

    动态挂载组件5：优化

--- src/lib/openDialog.ts
+++ src/lib/openDialog.ts
@@ -6,6 +6,10 @@ export const openDialog = (options) => {
   // 这里创建一个临时 div 放到 body 上
   const div = document.createElement('div')
   document.body.appendChild(div)
+  const close = () => {
+    app.unmount()
+    div.remove()
+  }
   // 定义名字方便销毁
   const app = createApp({
     render(){
@@ -15,8 +19,7 @@ export const openDialog = (options) => {
           visible: true,
           'onUpdate:visible': (newVisible) => { // 监听 visible 事件
             if(newVisible === false){
-              app.unmount()
-              div.remove()
+              close()
             }
           }
         },
