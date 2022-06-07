9465bad082d680bf39a14ccd187640bc73a430f1
lifeng <506076395@qq.com>
2022-06-07 18:04:17 +0800

    动态挂载组件4：点击关闭功能完成

--- src/lib/openDialog.ts
+++ src/lib/openDialog.ts
@@ -6,15 +6,27 @@ export const openDialog = (options) => {
   // 这里创建一个临时 div 放到 body 上
   const div = document.createElement('div')
   document.body.appendChild(div)
-  // 把 Dialog 挂载到 div 上
-  createApp({
+  // 定义名字方便销毁
+  const app = createApp({
     render(){
       return h(
         Dialog,
-        {visible: true},
+        {
+          visible: true,
+          'onUpdate:visible': (newVisible) => { // 监听 visible 事件
+            if(newVisible === false){
+              app.unmount()
+              div.remove()
+            }
+          }
+        },
         {title:title, content: content}
       )
     }
-  }).mount(div)
+  })
+  // vue 语法 必须跟上边是分开的
+  // 上边的 app 是CreateApp() 返回的 而不是 .mount(div) 返回的
+  app.mount(div)
 }
-// 传入 title 和 content
+// 这里的 visible:true 写死的 所以点击不能关闭 Dialog
+// 关闭 Dialog 也就是销毁 CreateApp 这里先在 div 卸载 再销毁 div
