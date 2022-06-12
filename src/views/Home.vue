02b3103f2df3afdbae2be1f36b4c1708128f7a7a
lifeng <506076395@qq.com>
2022-06-12 16:19:44 +0800

    Home 背景图文字样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,14 +2,18 @@
   <div class="topNavAndBanner">
     <TopNav/>
     <div class="banner">
-      <h1>咕 咕 UI</h1>
-      <h2>咕 咕 UI 框 架</h2>
+      <h1>布谷 UI</h1>
+      <h3 class="describe">一个简简单单的 UI 框架</h3>
       <p class="actions">
         <a href="https://github.com">GitHub</a>
         <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
+  <svg width="100%" height="100%" id="svg" viewBox="0 0 1440 100" class="transition duration-300 ease-in-out delay-150">
+    <path d="M 0,400 C 0,400 0,200 0,200 C 161.33333333333331,196 322.66666666666663,192 489,185 C 655.3333333333334,178 826.6666666666667,168 986,170 C 1145.3333333333333,172 1292.6666666666665,186 1440,200 C 1440,200 1440,400 1440,400 Z" stroke="none" stroke-width="0" fill="#dfe9f3ff" class="transition-all duration-300 ease-in-out delay-150 path-0" transform="rotate(-180 720 100)">
+    </path>
+  </svg>
 </template>
 
 <script lang="ts">
@@ -21,27 +25,35 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-$green:#02bcb0;
-$radius: 4px;
-$color: rgb(1 157 147);
+$buttonBlue: rgb(28 65 99);
+$radius: 8px;
+$color: rgb(61 87 111);
 .topNavAndBanner{
-  background-image: linear-gradient(-225deg, #FFFEFF 0%, #D7FFFE 100%);
+  background-image: linear-gradient(to top, #dfe9f3 0%, #f7fbff 100%);
 }
 .banner {
   color: $color;
-  padding: 100px 0;
+  padding: 130px 0 80px 0;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  > h1{
+    font-weight: 300;
+  }
+  > h3{
+    padding: 0 0 10px 0;
+  }
   > .actions {
     padding: 8px 0;
     a {
       margin: 0 8px;
-      background: $green;
+      padding: 8px;
+      background: $buttonBlue;
+      font-size: 16px;
+      line-height: 16px;
       color: white;
       display: inline-block;
-      padding: 8px 24px;
       border-radius: $radius;
       &:hover{
         text-decoration: none;
