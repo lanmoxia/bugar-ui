31c1e12f7404b41be10415c504335d7348234d4d
lifeng <506076395@qq.com>
2022-06-14 20:47:38 +0800

    改造 DialogDemo

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,44 +1,16 @@
 <template>
-  <div>Dialog 示例</div>
-  <h1>示例1</h1>
-    <Button @click="toggle">toggle</Button>
-    <Dialog v-model:visible="x" :close-on-click-overlay="false"
-            :ok="f1" :cancel="f2">
-      <template v-slot:content>
-        <strong>这里加粗的</strong>
-        <div>这里正常的</div>
-      </template>
-      <template v-slot:title>
-        <strong>这里是加粗的提示</strong>
-      </template>
-    </Dialog>
-  <h1>示例2</h1>
-  <Button @click="showDialog">show</Button>
+  <h1>Dialog 示例</h1>
+  <Demo :component="Dialog1Demo" />
+  <Demo :component="Dialog2Demo" />
 </template>
 <script>
-import Dialog from '../lib/Dialog.vue'
-import Button from '../lib/Button.vue'
-import {ref} from 'vue'
-import {openDialog} from '../lib/openDialog.ts'
+import Demo from './Demo.vue';
+import Dialog1Demo from './Dialog1.demo.vue';
+import Dialog2Demo from './Dialog2.demo.vue';
 export default {
-  components: {Button, Dialog},
+  components: {Demo},
   setup(){
-    const x = ref(false)
-    const toggle = () => {
-      x.value = !x.value
-    }
-    const f1 = () => {return false}
-    const f2 = () => {}
-    const showDialog = () => {
-      openDialog({
-        title: "标题",
-        content: "你好",
-        ok(){console.log('ok')},
-        cancel(){console.log('cancel')}
-      })
-    }
-    return{x, toggle, f1, f2, showDialog}
-
+    return {Dialog1Demo, Dialog2Demo}
   }
 }
 </script>
