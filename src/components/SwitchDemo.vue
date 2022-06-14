091d8d92fd1987e2ffea5611d8c0a2b07ec3b29a
lifeng <506076395@qq.com>
2022-06-14 15:46:28 +0800

    抽离用户需要拷贝的代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -4,19 +4,19 @@
     <div class="demo">
       <h2>常规用法</h2>
       <div class="demo-component">
-        <Switch v-model:value="bool" />
+        <Switch1Demo />
       </div>
       <div class="demo-actions">
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>&lt;Switch v-model:value="bool" /&gt;</pre>
+        <pre>&lt;Switch v-model:value="bool"/&gt;</pre>
       </div>
     </div>
     <div class="demo">
       <h2>支持 disabled </h2>
       <div class="demo-component">
-        <Switch v-model:value="bool" disabled />
+        <Switch2Demo />
       </div>
       <div class="demo-actions">
         <Button>查看代码</Button>
@@ -32,8 +32,11 @@
 import Switch from '../lib/Switch.vue';
 import Button from '../lib/Button.vue';
 import {ref} from 'vue';
+import Switch1Demo from '../components/Switch1.demo.vue'
+import Switch2Demo from '../components/Switch2.demo.vue'
+
 export default {
-  components: {Switch, Button},
+  components: {Switch1Demo, Switch2Demo, Switch, Button},
   setup(){
     const bool = ref(false)
     return {
@@ -63,6 +66,7 @@ $border-color: #d9d9d9;
     padding: 8px 16px;
     border-top: 1px dashed $border-color;
     >pre {
+      letter-spacing: 1px;
       line-height: 1.1;
       font-family: Consolas, 'Courier New', Courier, monospace;
       margin: 0;
