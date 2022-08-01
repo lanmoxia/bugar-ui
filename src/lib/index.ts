3f3782a5caf66a874c7f7d5d8d3acedf4fe34b97
lanmoxia <506076395@qq.com>
2022-08-01 14:18:54 +0800

    rollup：创建 lib/index.ts

---
+++ src/lib/index.ts
@@ -0,0 +1,15 @@
+// import Switch from './Switch.vue'
+// import Button from './Button.vue'
+// import Tabs from './Tabs.vue'
+// import Tab from './Tab.vue'
+// import {openDialog as openDialog} from './openDialog'
+//
+// export {Switch, Button, Tabs, Tab, openDialog}
+
+// 简写
+export {default as Switch} from './Switch.vue'
+export {default as Button} from './Button.vue'
+export {default as Tabs} from './Tabs.vue'
+export {default as Tab} from './Tab.vue'
+export {openDialog as openDialog} from './openDialog'
+
